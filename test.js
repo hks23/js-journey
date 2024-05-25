04044b03f5faa4e20d0ff61e418e7cfab095d45d
Harsh Kr Singh <harshks.mit@gmail.com>
2024-05-25 18:10:36 +0530

    data types

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-console.log("harsh")
-console.log("hello")
+"use strict"; //treat all JS code as newer version>
 
-console.log("test1")
-console.log("test2");
+console.log("lets start the hustle")
+//4 
+const accountId = 1234
+let accountState = 200
+console.table([accountId , accountState]) //this prints a table with key value pair of all variables
 
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined => no value defined not same as null
+//symbol => unique
+//object
+console.log(typeof null) //returns OBJECT but
+console.log(typeof undefined) //returns undefined only
